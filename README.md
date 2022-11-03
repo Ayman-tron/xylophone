f4bfec20f5ba35e7fd48055eeffaa82df8c33d12
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-11-03 19:59:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Xylophone 🎹
+
+## A Xylophone app that plays sounds.  
+
+<img src="images/xylophone.jpeg"  width="230.5" height="512">
+
+## This project allowed me to learn the following:
+
+- Incorporating open source libraries of code into my project using Flutter Packages.
+- Playing sound on both iOS and Android.
+- Generating repeated user interface Widgets.
+- Using Dart functions that can take input arguments as well as return an output.
+- Dart arrow syntax for writing one line functions.
+
+
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({color, soundNum}) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            playSound(soundNum);
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNum: 1),
+              buildKey(color: Colors.orange, soundNum: 2),
+              buildKey(color: Colors.yellow, soundNum: 3),
+              buildKey(color: Colors.green, soundNum: 4),
+              buildKey(color: Colors.teal, soundNum: 5),
+              buildKey(color: Colors.blue, soundNum: 6),
+              buildKey(color: Colors.purple, soundNum: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
